98103048c25147f51850342ac2a690e0df6fe1c7
yaiangel13 <12645830+yaiangel13@users.noreply.github.com>
2024-10-29 16:43:52 +0100

    test push colaborador

--- assets/navbar/vocabulario.js
+++ assets/navbar/vocabulario.js
@@ -1,7 +1,7 @@
 menu.vocabulario = {
     hangul: [],
     bloque: [
-        {id: 'test', title: 'large name test'},
+        {id: 'test', title: 'large name test 1'},
         {id: 'test2', title: 'very large korean 안녕 test'}
     ]
 };
