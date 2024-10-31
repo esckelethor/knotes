803b7b0e926645dc39af72e3286ff57237eeb674
esckelethor <26528445+esckelethor@users.noreply.github.com>
2024-10-31 12:46:07 +0100

    estructura navbar verbosy/frases, borrar archivos de test

--- assets/navbar/frases.js
+++ assets/navbar/frases.js
@@ -1,5 +1,41 @@
 menu.frases = {
-    general: [
-        {id: 'test', title: 'large name test'}
-    ]
+    felicitaciones: [
+        {id: 'regalo', title: 'Regalos'},
+        {id: 'navidad', title: 'Feliz navidad'}
+    ],
+    "sacando tema": [
+        {id: 'que-haces', title: '¿Qué haces?'},
+        {id: 'ocupado', title: 'Estar o no ocupad@'},
+        {id: 'que-hora', title: '¿Qué hora es?'},
+        {id: 'que-hobby', title: '¿Cuál es tu hobby?'},
+        {id: 'favorito', title: '¿Cuál es tu favorito?'},
+        {id: 'que-piensas', title: '¿En qué piensas?'},
+        {id: 'buen-dia', title: '¿Has tenido un buen día?/Ten un buen día'},
+        {id: 'despierto', title: '¿Qué haces despiert@ a estas horas?'}
+    ],
+    "hablando del clima": [],
+    "respuestas base": [],
+    "otras expresiones": [
+        {id: 'no-poder', title: 'No puede ser/No debes/Es imposible'},
+        {id: 'ser-muchos', title: 'Ser/Haber muchos'}
+    ],
+    disculpas: [],
+    ayuda: [
+        {id: 'ayudar', title: 'Ayudar'},
+        {id: 'correccion', title: 'Gracias por la corrección'},
+        {id: 'respuesta-tarde', title: 'Perdón por mi respuesta tardía'},
+        {id: 'que-es', title: '¿Qué es?'},
+        {id: 'entendido', title: '¿Has entendido todo lo que he dicho?'}
+    ],
+    "hablar en idioma" : [
+        {id: 'por-que-tema', title: '¿Por qué [tema]?'},
+        {id: 'hablar-idioma', title: 'No sé hablar/Puedo hablar [idioma]'},
+        {id: 'estudio-idioma', title: 'Estudio [idioma]'},
+        {id: 'como-se-dice', title: '¿Cómo se dice [esto] en [idioma]?'}
+    ],
+    'estancia en lugar': [],
+    suerte: [],
+    amistad: [],
+    amor: [],
+    desamor: []
 };
