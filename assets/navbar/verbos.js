c5adb7987a36bf2e504cac5daad4fe3c0020cc25
esckelethor <26528445+esckelethor@users.noreply.github.com>
2024-10-31 12:46:07 +0100

    estructura navbar verbosy/frases, borrar archivos de test

--- assets/navbar/verbos.js
+++ assets/navbar/verbos.js
@@ -1,5 +1,20 @@
 menu.verbos = {
     principales: [
-        {id: 'test', title: 'large name test'}
+        {id: '이다', title: '이다 ~ Ser/Estar'},
+        {id: '아니다', title: '아니다 ~ No ser/No estar'},
+        {id: '있다', title: '있다 ~ Existencia/Posesión'},
+        {id: '없다', title: '없다 ~ No existencia/No posesión'},
+        {id: 'pasado-이다-아니다', title: 'Pasado de 이다 y 아니다'}
+    ],
+    regulares: [
+        {id: '하다', title: '하다 ~ Hacer'}
+    ],
+    irregulares: [
+        {id: '-ㅂ', title: 'Finalizado en -ㅂ'},
+        {id: '-ㄷ', title: 'Finalizado en -ㄷ'},
+        {id: '-ㄹ', title: 'Finalizado en -ㄹ'},
+        {id: '-르', title: 'Finalizado en -르'},
+        {id: '-ㅅ', title: 'Finalizado en -ㅅ'},
+        {id: 'tabla-르', title: 'Tabla verbos -르'}
     ]
 };
