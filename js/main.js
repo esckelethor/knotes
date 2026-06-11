33ef4f43d29cb02a47cef73e3d37d7ee3e451970
esckelethor <26528445+esckelethor@users.noreply.github.com>
2026-06-11 20:53:41 +0200

    close #16 make lang menu dynamic, refactor and update CSS rules

--- js/main.js
+++ js/main.js
@@ -13,6 +13,60 @@ checkVisibility = function(pSelector) {
     $v(pSelector).css('visibility', (vVisibility == 'hidden') ? 'visible' : 'hidden');
 }
 
+loadLangMenu = function () {
+   Object.entries(gMenu).forEach(([pKey, pValue]) => {
+        //if not lang menu skip
+        if (pKey.length != 2) return;
+
+        //create lang main div
+        var vLangCssClasses = ['lang-item'];
+        if (Object.entries(pValue).length == 0) vLangCssClasses.push('lang-disabled');
+        if (pKey == gLang) vLangCssClasses.push('lang-selected');
+
+        var vLangMainElement = $v().createElement({
+            label: 'div',
+            id: 'lang-' + pKey,
+            classes: vLangCssClasses
+        });
+        
+        //create lang img
+        var vLangImgElement = $v().createElement({
+            label: 'img',
+            attrs: [{attr: 'src', value: '../assets/img/flags/' + pKey + '.png'}, {attr: 'alt', value: 'lang-' + pKey}]
+        });
+        
+        //create lang title
+        var vLangTitleElement = $v().createElement({
+            label: 'div',
+            innerHTML: pKey.toUpperCase()
+        });
+        
+        //append to lang menu
+        $v('#lang-selector').appendChilds(vLangMainElement);
+        $v('#lang-selector div:last-child').appendChilds([vLangImgElement, vLangTitleElement]);
+    });
+
+    //add click events
+    $v('.lang-item').addEvent('click', (pEvent) => {
+        //change selected lang
+        $v('#lang-selector .lang-selected').removeClass('lang-selected');
+        $v('#' + pEvent.currentTarget.id).addClass('lang-selected');
+        
+        //update current lang on footer
+        gLang = pEvent.currentTarget.id.split('-')[1];
+        var vSrcLang = $v('#' + pEvent.currentTarget.id + '>img').attr('src');
+        var vTxtLang = $v('#' + pEvent.currentTarget.id + '>div').innerHTML();
+        $v('#lang>img').attr('src', vSrcLang);
+        $v('#lang>div').innerHTML(vTxtLang);
+        
+        //reload menus & spotify playlists
+        loadMenu();
+        loadSpotifyLists();
+        gSpotifyEmbedController.loadUri(gMenu.spotify[gLang][0].spotifyID);
+        $v('#lang-selector').css('visibility', 'hidden');
+    });
+}
+
 $v('.sam_taeguk_dj').addEvent('click', () => {
     //auto play on first visibility change
     if (gCurrentSpotifyPlaylist == null) {
@@ -30,18 +84,6 @@ $v('#version').addEvent('click', (pEvent) => {
 
 $v('#lang').addEvent('click', (pEvent) => {
     checkVisibility('#lang-selector');
-});
-
-$v('.lang-item').addEvent('click', (pEvent) => {
-    gLang = pEvent.currentTarget.id.split('-')[1];
-    var vSrcLang = $v('#' + pEvent.currentTarget.id + '>img').attr('src');
-    var vTxtLang = $v('#' + pEvent.currentTarget.id + '>div').innerHTML();
-    $v('#lang>img').attr('src', vSrcLang);
-    $v('#lang>div').innerHTML(vTxtLang);
-    loadMenu();
-    loadSpotifyLists();
-    gSpotifyEmbedController.loadUri(gMenu.spotify[gLang][0].spotifyID);
-    $v('#lang-selector').css('visibility', 'hidden');
 });
 
 window.onSpotifyIframeApiReady = (IFrameAPI) => {
@@ -79,6 +121,7 @@ window.onSpotifyIframeApiReady = (IFrameAPI) => {
 };
 
 window.onload = function () {
+    loadLangMenu();
     loadMenu();
     $v().getProjectBuildVersion();
     loadSpotifyLists();
